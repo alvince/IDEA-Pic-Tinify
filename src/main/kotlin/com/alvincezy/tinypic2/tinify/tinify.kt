91954c1e246b5895dbb419d5c9925873d26bab34
alvince <alvince.zy@gmail.com>
2018-08-17 23:10:36 +0800

    Add function backup source file

--- src/main/kotlin/com/alvincezy/tinypic2/tinify/tinify.kt
+++ src/main/kotlin/com/alvincezy/tinypic2/tinify/tinify.kt
@@ -1,8 +1,11 @@
 package com.alvincezy.tinypic2.tinify
 
 import com.alvincezy.tinypic2.TinyPicOptionsConfigurable
+import com.alvincezy.tinypic2.exts.supportTinify
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.tinify.Tinify
+import java.io.File
 
 fun prepare(project: Project?, apiKey: String): Boolean {
     if (apiKey.isEmpty()) {
@@ -11,4 +14,20 @@ fun prepare(project: Project?, apiKey: String): Boolean {
     }
     Tinify.setKey(apiKey)
     return true
+}
+
+fun backupTinifySource(file: VirtualFile) {
+    if (file.supportTinify()) {
+        val backupDir = File("${file.parent.path}/backup")
+        if (!backupDir.exists()) {
+            backupDir.mkdirs()
+        }
+
+        val target = File(backupDir, file.name)
+        if (target.exists()) {
+            return
+        }
+
+        File(file.path).copyTo(target, true)
+    }
 }
